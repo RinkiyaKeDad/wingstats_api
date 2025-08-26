4e48a9cd5bb264a3fa70989bb2f6bbe674f7bf4e
Arsh Sharma <arshsharma461@gmail.com>
2025-08-26 21:34:50 +0530

    getting a json response on sending a post req
    Signed-off-by: Arsh Sharma <arshsharma461@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-use axum::{Router, routing::get};
+use axum::{Router, response::Json, routing::post};
+use serde_json::Value;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/", post(save_stats));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn save_stats(Json(payload): Json<serde_json::Value>) -> Json<Value> {
+    Json(payload)
 }
