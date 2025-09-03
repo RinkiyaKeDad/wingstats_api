49dd41ab5555349f8f65fa400b8b3d7a43abc327
Arsh Sharma <arshsharma461@gmail.com>
2025-09-03 11:49:06 +0530

    db connection successful
    Signed-off-by: Arsh Sharma <arshsharma461@gmail.com>

---
+++ README.md
@@ -0,0 +1,5 @@
+# Wingstats API
+
+Instructions for testing:
+
+- `curl -X POST -H "Content-Type: application/json" -d '{"someone": "60", "arsh": "110"}' http://localhost:3000/`
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,35 @@
 use axum::{Router, response::Json, routing::post};
+use dotenv::dotenv;
 use serde_json::Value;
+use sqlx::mysql::{MySqlPool, MySqlPoolOptions};
+use std::sync::Arc;
+
+pub struct AppState {
+    db: MySqlPool,
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", post(save_stats));
+    dotenv().ok();
+    let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = match MySqlPoolOptions::new()
+        .max_connections(10)
+        .connect(&db_url)
+        .await
+    {
+        Ok(pool) => {
+            println!("Connected to DB successfully");
+            pool
+        }
+        Err(err) => {
+            println!("Failed to connect to DB: {}", err);
+            std::process::exit(1);
+        }
+    };
+
+    let app = Router::new()
+        .route("/", post(save_stats))
+        .with_state(Arc::new(AppState { db: pool.clone() }));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
